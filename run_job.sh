56f6667ed98cf016308ea217fc2484908be212d8
harsilspatel <harsilspatel@gmail.com>
2019-09-19 04:14:53 +1000

    change log file naming conventions and switch to full paramater names

--- run_job.sh
+++ run_job.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-#!/bin/bash
-#SBATCH -N 2 # allocate 2 nodes for the job
-#SBATCH --ntasks-per-node=20
-#SBATCH --exclusive # no other jobs on the nodes while job is running
-#SBATCH -o fluentP_%J.out # the file to write stdout for fluent job
-#SBATCH -e fluentP_%J.err # the file to write stderr for fluent job
+#SBATCH --nodes=1 				# allocate 1 node for the job
+#SBATCH --ntasks-per-node=4
+#SBATCH --exclusive 			# no other jobs on the nodes while job is running
+#SBATCH --error="%x.err" 		# the file to write stderr for fluent job
+#SBATCH --output="%x.out" 		# the file to write stdout for fluent job
+#SBATCH --partition=short
+#SBATCH --ntasks=4
 
 JOURNAL=$1
 
