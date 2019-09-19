3966abcde58bbc36eab94f836aa7915f6c1f0bcd
harsilspatel <harsilspatel@gmail.com>
2019-09-19 13:32:07 +1000

    add params to sync to remote drive

--- scripts/run_all.sh
+++ scripts/run_all.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
 print_usage() {
-	echo "Usage: $(basename $0) <journalDirectory> <timelimit>"
+	echo "Usage: $(basename $0) <journalDirectory> <timelimit> <remoteResultDir>"
 }
 
-if [ "$#" -ne 2 ]; then
+if [ "$#" -ne 3 ]; then
 	print_usage;
 	exit 1;
 fi
 
 DIR=$1
 TIMELIMIT=$2
+REMOTE_RESULT_DIR=$3
 JOURNALS="$(find $DIR -name "*.jou" | sort)"
 
 
 for journal in $JOURNALS
 do
-	echo $journal
 	bodyname="${journal%\.*}"
-	jobname="$(basename $bodyname)"
+	jobname="$(basename $bodyname)"	
 	
-	sbatch --time=$TIMELIMIT --job-name=$jobname --output="$bodyname.out.txt" --error="$bodyname.err.txt" ./run_job.sh $bodyname 
-	
+	sbatch --time=$TIMELIMIT --job-name=$jobname --output="$bodyname.out.txt" --error="$bodyname.err.txt" ./run_job.sh $journal $REMOTE_RESULT_DIR
+		
 done
--- scripts/run_job.sh
+++ scripts/run_job.sh
@@ -6,10 +6,16 @@
 #SBATCH --partition=short
 #SBATCH --ntasks=4
 
+
 JOURNAL=$1
+BODY_NAME="${JOURNAL%\.*}"
+DIR_NAME="$(basename $BODY_NAME)"
+REMOTE_RESULT_DIR=$2
 
 # loading the ansys module which will
 # allow us to execute the `fluent` command.
 module load ansys/18.1
 
-fluent 3ddp -g -t${SLURM_NTASKS} -mpi=ibmmpi -i "$JOURNAL.jou" -ssh > "$JOURNAL.log"
+fluent 3ddp -g -t${SLURM_NTASKS} -mpi=ibmmpi -i $JOURNAL -ssh > "$BODY_NAME.log"
+
+rclone copy "$BODY_NAME*" "hpat0003:$REMOTE_RESULT_DIR/$DIR_NAME"
